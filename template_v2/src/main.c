5da66fe5a3021f4d92e1902f272c5e695e07e670
Blanpain_Thibault <thibault.blanpain@student.uclouvain.be>
2019-04-20 12:35:50 +0200

    etapes

--- template_v2/src/main.c
+++ template_v2/src/main.c
@@ -4,5 +4,12 @@
 #include "reverse.h"
 #include "sha256.h"
 int main(int argc, char **argv){
+  /*
+  les etapes du programme:
+  1- lire les fichiers
+  2- utiliser la fonction reversehash(const uint8_t *hash, char *res, size_t len);
+  3- trier
+  4- display
+  */
     return EXIT_SUCCESS;
 }
