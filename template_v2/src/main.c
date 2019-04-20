f85ec219b2f32ba8673f9c287f7fa6d0c7858818
Blanpain_Thibault <thibault.blanpain@student.uclouvain.be>
2019-04-20 13:31:06 +0200

    fv

--- template_v2/src/main.c
+++ template_v2/src/main.c
@@ -8,13 +8,14 @@
 int main(int argc, char **argv){
   /*
   les etapes du programme:
+  0- lire les arguments
   1- lire les fichiers
   2- utiliser la fonction reversehash(const uint8_t *hash, char *res, size_t len);
   3- trier
-  4- display ligne par ligne 
+  4- display ligne par ligne
   */
 
-
+  
 
 
   return EXIT_SUCCESS;
