d1c00a296ce5ae5a1323e2754f12148d810bd17b
nat1236 <natacha.goux@uclouvain.be>
2019-04-27 10:14:28 +0200

    jik

--- template_v2/src/main.c
+++ template_v2/src/main.c
@@ -66,12 +66,12 @@ seront pas d office des int ou char*) */
   /* maintenant que les fichiers a lire (.bin) sont stockes dans un tableau, il
   faut les differencier, selon leur provenance et les lire */
 
-  int pthread_create(pthread_t * thread, NULL, void *(*start_routine) (void *), void *arg)
+  int pthread_create(pthread_t * thread, NULL, void *(*lecture) (void *), void *arg)
 
   return EXIT_SUCCESS;
 }
 
-int lecture() { //focntion de lecture 
+int lecture() { //focntion de lecture
   int i ;
   int placeFich = 0;
   char fichs[argc];
